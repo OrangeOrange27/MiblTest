8e51ca8cb0eb330a5569c380f21ff35b1169d057
Dan <danildanilchenko.27@gmail.com>
2023-03-20 19:30:23 +0300

    Reworked player movement

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -8,6 +8,7 @@ namespace Player
         [SerializeField] private PlayerInput _playerInput;
         [SerializeField] private float _playerSpeed = 10f;
         [SerializeField] private float _smoothInputSpeed = .2f;
+        [SerializeField] private float _playerAcceleration = 2.5f;
 
         public InputAction MoveAction { get; private set; }
         public InputAction LookAction { get; private set; }
@@ -15,8 +16,12 @@ namespace Player
         private Vector2 _currentInputVector;
         private Vector2 _smoothInputVelocity;
 
+        private Rigidbody _rigidbody;
+
         private void Awake()
         {
+            _rigidbody = GetComponent<Rigidbody>();
+            
             MoveAction = _playerInput.actions["Move"];
             LookAction = _playerInput.actions["Look"];
         }
@@ -32,8 +37,11 @@ namespace Player
             _currentInputVector = Vector2.SmoothDamp(_currentInputVector, input, ref _smoothInputVelocity, _smoothInputSpeed);
             
             var moveVector = new Vector3(_currentInputVector.x, 0, _currentInputVector.y);
-            moveVector *= _playerSpeed * Time.deltaTime;
-            transform.Translate(moveVector,Space.World);
+            moveVector *= Mathf.Pow(_playerSpeed,_playerAcceleration) * Time.deltaTime;
+
+            //move
+            //transform.Translate(moveVector,Space.World);
+            _rigidbody.AddForce(moveVector, ForceMode.Force);
         }
     }
 }
